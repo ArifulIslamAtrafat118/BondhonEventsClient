2ec99725133a3b578a0650f00d9803e5fa4427be
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-08-10 21:34:35 +0600

    bug fix: blank sign-in page(after wrong credintial) is resolved

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -24,38 +24,43 @@ const SignIn = () => {
   const { SwalTheme } = useSwalTheme();
   const navigate = useNavigate();
   const location = useLocation();
-  const from = location?.state || "/";
+  const from =
+    location.state?.pathname && typeof location.state.pathname === "string"
+      ? location.state.pathname
+      : "/";
+  console.log(from);
   const navigateNow = () => {
-    setTimeout(() => {
-      navigate(from, { replace: true });
-    }, 1);
+    navigate(from, { replace: true });
   };
 
-  const handleLogin = async (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    try {
-      await login(email, password);
-      Swal.fire({
-        icon: "success",
-        text: "Sign-In to Bondhon Events successful.",
-        timer: 800,
-        title: "Congratulaions!",
-        ...SwalTheme,
-      }).then(() => {
-        navigateNow();
+
+    login(email, password)
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          text: "Sign-In to Bondhon Events successful.",
+          timer: 800,
+          title: "Congratulaions!",
+          ...SwalTheme,
+        }).then(() => {
+          navigateNow();
+          console.log("hello I am trying to move...");
+        });
+      })
+      .catch((error) => {
+        console.log("console error", error.message);
+        // errorToast(error);
+        Swal.fire({
+          icon: "error",
+          title: "Sign-In Failed",
+          text: `${error.message}`,
+          ...SwalTheme,
+        }).then(() => {
+          window.location.reload();
+        });
       });
-    } catch (error) {
-      console.log("console error", error.message);
-      errorToast(error);
-      // Swal.fire({
-      //   icon: "error",
-      //   title: "Sign-In Faild",
-      //   text: `${error.message}`,
-      //   ...SwalTheme,
-      // }).then(() => {
-      //   navigate("/sign-in");
-      // });
-    }
   };
 
   const handleGoogleSignIn = async () => {
@@ -77,67 +82,65 @@ const SignIn = () => {
     }
   };
 
-  if (!currentUser) {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        className="relative min-h-screen  flex items-center justify-center bg-green-50 dark:bg-gray-900 px-4 overflow-hidden"
-      >
-        <Lottie
-          animationData={gardenAnim}
-          loop={true}
-          className="absolute inset-0 w-full h-full object-cover opacity-50 pointer-events-none"
-        />
-        <div className="relative z-10 bg-gray-100 dark:bg-gray-800 p-8 rounded-lg shadow-lg max-w-md w-full">
-          <h2 className="text-2xl font-semibold text-center mb-6 text-gray-800 dark:text-white">
-            Welcome Back
-          </h2>
-          <form onSubmit={handleLogin} className="space-y-4">
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 rounded bg-white dark:bg-gray-700 dark:text-white text-gray-900 border border-gray-300 dark:border-gray-600"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 rounded bg-white dark:bg-gray-700 text-gray-900 dark:text-white border border-gray-300 dark:border-gray-600"
-              required
-            />
-            <button
-              type="submit"
-              className="w-full bg-green-500 hover:bg-green-600 text-white py-2 rounded cursor-pointer"
-            >
-              Sign In
-            </button>
-          </form>
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="relative min-h-screen  flex items-center justify-center bg-green-50 dark:bg-gray-900 px-4 overflow-hidden"
+    >
+      <Lottie
+        animationData={gardenAnim}
+        loop={true}
+        className="absolute inset-0 w-full h-full object-cover opacity-50 pointer-events-none"
+      />
+      <div className="relative z-10 bg-gray-100 dark:bg-gray-800 p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl font-semibold text-center mb-6 text-gray-800 dark:text-white">
+          Welcome Back
+        </h2>
+        <form onSubmit={handleLogin} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 rounded bg-white dark:bg-gray-700 dark:text-white text-gray-900 border border-gray-300 dark:border-gray-600"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 rounded bg-white dark:bg-gray-700 text-gray-900 dark:text-white border border-gray-300 dark:border-gray-600"
+            required
+          />
           <button
-            onClick={handleGoogleSignIn}
-            className="w-full bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 text-gray-700 dark:text-white py-2 rounded mt-3 cursor-pointer"
+            type="submit"
+            className="w-full bg-green-500 hover:bg-green-600 text-white py-2 rounded cursor-pointer"
           >
-            Sign in with Google
+            Sign In
           </button>
-          <p className="text-sm mt-4 text-center text-gray-700 dark:text-gray-300">
-            Don’t have an account?{" "}
-            <Link
-              to="/sign-up"
-              state={location.state}
-              className="text-green-600 hover:underline"
-            >
-              Register here
-            </Link>
-          </p>
-        </div>
-      </motion.div>
-    );
-  }
+        </form>
+        <button
+          onClick={handleGoogleSignIn}
+          className="w-full bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 text-gray-700 dark:text-white py-2 rounded mt-3 cursor-pointer"
+        >
+          Sign in with Google
+        </button>
+        <p className="text-sm mt-4 text-center text-gray-700 dark:text-gray-300">
+          Don’t have an account?{" "}
+          <Link
+            to="/sign-up"
+            state={location.state}
+            className="text-green-600 hover:underline"
+          >
+            Register here
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default SignIn;
